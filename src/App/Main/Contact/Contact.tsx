4b68f2144be5317cc64f320f890c0620a8a8e4a4
Emily Jackson <ejackson@evolvtechnology.com>
2024-11-22 18:02:04 -0500

    Create Homepage
    - Create home page components
    - no functionality
    - Set custom tailwind colors and corner radius

---
+++ src/App/Main/Contact/Contact.tsx
@@ -0,0 +1,5 @@
+const Contact = (): JSX.Element => {
+  return <div>Contact</div>
+}
+
+export default Contact
--- src/App/Main/Footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = (): JSX.Element => {
-  return(
-    <div className="bottom-0 w-full bg-gray-100 text-sm">This is the Footer</div>
-  )
-}
-
-export default Footer
