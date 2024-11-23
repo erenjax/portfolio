fac598e59cb322f765efaa2f9aed129f3ed729e8
Emily Jackson <ejackson@evolvtechnology.com>
2024-11-22 21:16:59 -0500

    Contact
    **I might not need a contact page, will do last

--- src/App/Main/Contact/Contact.tsx
+++ src/App/Main/Contact/Contact.tsx
@@ -1,5 +1,13 @@
 const Contact = (): JSX.Element => {
-  return <div>Contact</div>
+  return (
+    <div className="flex flex-col w-screen bg-customBlack min-h-screen">
+      <div className="flex w-full justify-center md:pb-8">
+        <div className="flex w-fit bg-customWhite py-4 px-24">
+          <p className="text-xl text-customBlack">Contact</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
